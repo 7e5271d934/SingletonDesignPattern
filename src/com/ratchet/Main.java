73626fdcb4bc741064c87b429a7058a22307c010
7e5271d934 <HBuqLEP2YebfD0ZAr1of@protonmail.com>
2021-01-10 13:13:48 -0100

    Singleton class implemented

--- src/com/ratchet/Main.java
+++ src/com/ratchet/Main.java
@@ -3,7 +3,7 @@ package com.ratchet;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("The total is: " + Singleton.getInstance().getTotal());
     }
 
 }
